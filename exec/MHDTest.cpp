f611889b4751ecf9a284c8baa4d28742e15fd8e4
dhami1234 <55599148+dhami1234@users.noreply.github.com>
2023-05-22 13:21:25 -0500

    minor

--- exec/MHDTest.cpp
+++ exec/MHDTest.cpp
@@ -115,7 +115,6 @@ int main(int argc, char* argv[])
 		if (inputs.grid_type_global == 2){
 				MHD_Mapping::Spherical_2O_map_filling_func(state);
 				MHD_Mapping::Spherical_map_filling_func(state);
-				MHD_Mapping::Spherical_map_filling_func2(state);
 		} else {
 			MHD_Mapping::Regular_map_filling_func(state);
 		}
